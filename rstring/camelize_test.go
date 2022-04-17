09dfc87c86fc2210eb03a65c73345d554a5caefb
jimxl <tianxiaxl@gmail.com>
2022-04-17 14:58:28 +0800

    修正了拼写的错误

--- rstring/camelize_test.go
+++ rstring/camelize_test.go
@@ -7,7 +7,7 @@ import (
 )
 
 var _ = Describe("Camelize", func() {
-	It("单词是已下划线分割的时候，能返回正确的值", func() {
+	It("单词是以下划线分割的时候，能返回正确的值", func() {
 		str := rstring.Camelize("abc_def")
 		Expect(str).To(Equal("AbcDef"))
 	})
